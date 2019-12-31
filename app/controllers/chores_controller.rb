3084c51498edf2f7a677735f0f77f5aa806fd560
github username <github email address>
2019-12-31 15:04:27 -0800

    adding post to signup

--- app/controllers/chores_controller.rb
+++ app/controllers/chores_controller.rb
@@ -2,6 +2,7 @@ class ChoresController < ApplicationController
 
     get '/chores' do 
         authorize
+        @chores = Chore.all
         erb :'chores/index'
     end
     
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,7 +2,7 @@ class SessionsController < ApplicationController
     
     #Routes for user to login
     get '/login' do
-        redirect '/chores' if logged_in?
+        redirect '/home' if logged_in?
         @failed = false
         erb :'sessions/login'
     end
@@ -11,7 +11,7 @@ class SessionsController < ApplicationController
         user = User.find_by(username: params[:username])
         if !!user && user.authenticate(params[:password])
             session[:user_id] = user.id
-            redirect '/chores'
+            redirect '/home'
         else 
             @failed = true
             erb :'sessions/login' 
@@ -20,11 +20,18 @@ class SessionsController < ApplicationController
 
     #Routes for user to signup
     get '/signup' do
+        redirect '/home' if logged_in?
         erb :'sessions/signup'
     end
 
-    post '/signup' do
-        
+    post '/users' do
+        @user = User.create(username: params[:username], password: params[:password])
+        if @user.errors.any?
+            erb :'sessions/signup'
+        else
+            session[:user_id] = @user.id
+            redirect '/home'
+        end
     end
 
 end
